02b7fd0db9a2e057ab98ef2ae2c65bdfb86de107
leiton <sebastian.leiton@uao.edu.co>
2026-03-04 12:19:07 -0500

    ✨Implement: Implement all of the Contact logic into the main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import contactsLogo from './assets/contacts.svg'
+import Loader from './components/Loader'
+import type { Contact } from './components/Contacts'
 import './App.css'
+import ContactList from './components/DisplayList'
+import AddContacts from './components/AddContact'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+  const [contacts, setContacts] = useState<Contact[]>([])
+  const addContact = (newContact: Omit<Contact, "id">) => {
+  const contactWithId: Contact = {
+    id: Date.now(), // generamos id único
+    ...newContact
+  };
 
+  setContacts([...contacts, contactWithId]);
+};
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+
+      setContacts([
+        { id: 1, name: 'Roberto Carlos', number: '3001234567' },
+        { id: 2, name: 'Samuel De Luque', number: '3117654321' },
+        { id: 3, name: 'Magali Chavez', number: '3003434587'}
+      ])
+    }, 1000)
+  }, [])
+
+  const deleteContact = (id: number) => {
+    setContacts(contacts.filter(contact => contact.id !== id))
+  }
+
+  if (loading) {
+    return <Loader/>
+  }
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <img src={contactsLogo} className="logo" alt="Contacts logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Contacts App</h1>
+      <h2>Bienvenido a su aplicación de contactos confiable</h2>
+      <AddContacts addContact={addContact}/>
+      <h3>Estos son sus contactos:</h3>
+      <ContactList contacts={contacts} deleteContact={deleteContact}/>
     </>
   )
 }
