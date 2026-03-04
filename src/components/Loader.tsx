e6d6c2e99ed27c69d93c52db997d44e990f068a1
leiton <sebastian.leiton@uao.edu.co>
2026-03-04 12:15:57 -0500

    ✨Add: Add Loader file to display the loading text

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+function Loader() {
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h2>Cargando App</h2>
+        </div>
+    );
+}
+
+export default Loader;
