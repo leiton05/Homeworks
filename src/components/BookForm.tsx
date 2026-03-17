4f165699ba4a4fa619d536c1edc2530e09a37c9a
leiton <sebastian.leiton@uao.edu.co>
2026-03-17 15:08:53 -0500

    ✨Feat: Create components folder for the diferent elements to render in the project

---
+++ src/components/BookForm.tsx
@@ -0,0 +1,25 @@
+export function BookForm() {
+    return (
+        <>
+            <form action="">
+                <input 
+                type="text"
+                placeholder="Nombre"
+                />
+                <input 
+                type="text"
+                placeholder="ISBN"
+                />
+                <input 
+                type="text"
+                placeholder="Autor"
+                />
+                <input 
+                type="text"
+                placeholder="Editorial"
+                />
+                <button> Agregar </button>
+            </form>
+        </>
+    )
+}
