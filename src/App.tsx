d3cbea978726ebe9f977f1e85341cfb7ce2bcca1
leiton <sebastian.leiton@uao.edu.co>
2026-03-17 21:50:47 -0500

    ✨Feat: Update App.tsx to show the booklist and to convert the mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import './App.css'
-import { BookForm } from './components/BookForm'
-import { BookList } from './components/BookList'
-import { mockData } from './data/mockData'
+import "./App.css";
+import { BookForm } from "./components/BookForm";
+import { BookList } from "./components/BookList";
+import { mockData } from "./data/mockData";
+import type { Book } from "./models/Book";
+import type Stack from "./tools/Stack";
+import { ArrayToStacks } from "./tools/StackMethods";
 
 /* console.log(mockData) */
+
+/* Convierto el Arreglo de la mockData de los libros y lo paso 
+a que sea un Stack */
+const BookStack: Stack<Book> = ArrayToStacks(mockData);
+console.log(BookStack.print());
 
 function App() {
   return (
     <>
       <h1>BOOKSTACKS</h1>
       <p>Coloca tus libros para luego facilmente sacarlos</p>
-      <BookForm/>
-      <p>Libros en espera:</p>
-      {/* <BookList/> */}
+      <BookForm />
+      <h3>Libros en espera:</h3>
+      <BookList book={BookStack} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
