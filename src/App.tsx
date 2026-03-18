f1bc00ba3fbcb23ffb206a21c2aadd3cc8183b4f
leiton <sebastian.leiton@uao.edu.co>
2026-03-17 19:51:31 -0500

    🚧Construction: Create the initial structure of the whole page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
 import { BookForm } from './components/BookForm'
+import { BookList } from './components/BookList'
+import { mockData } from './data/mockData'
+
+/* console.log(mockData) */
 
 function App() {
   return (
     <>
-      <h1>PILAS Y COLAS</h1>
+      <h1>BOOKSTACKS</h1>
+      <p>Coloca tus libros para luego facilmente sacarlos</p>
       <BookForm/>
+      <p>Libros en espera:</p>
+      {/* <BookList/> */}
     </>
   )
 }
