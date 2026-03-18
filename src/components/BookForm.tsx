90e88d3a049d5bbff1ebf51a3e4c9154f39d07de
leiton <sebastian.leiton@uao.edu.co>
2026-03-18 00:08:40 -0500

    ✨Feat: Add Handle submit and take book logic

--- src/components/BookForm.tsx
+++ src/components/BookForm.tsx
@@ -1,18 +1,86 @@
-export function BookForm() {
+import { useState } from "react";
+import type { Book } from "../models/Book";
+import Stack from "../tools/Stack";
+
+interface BookFormProp {
+  stack: Stack<Book>;
+  setStack: (stack: Stack<Book>) => void;
+}
+
+export function BookForm({ stack, setStack }: BookFormProp) {
+  const [name, setName] = useState("");
+  const [isbn, setIsbn] = useState("");
+  const [author, setAuthor] = useState("");
+  const [editorial, setEditorial] = useState("");
+
+  const takeBook = () => {
+    if (stack.isEmpty()) {
+      console.log("No hay libros restantes");
+      return;
+    }
+
+    const items = stack.getItems();
+    items.pop();
+    const newStack = new Stack<Book>(items);
+    setStack(newStack);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    console.log("Submiiiiiit");
+
+    e.preventDefault();
+
+    const newBook: Book = {
+      name: name,
+      isbn: isbn,
+      author: author,
+      editorial: editorial,
+    };
+
+    const newStack = new Stack<Book>([...stack.getItems(), newBook]);
+    setStack(newStack);
+
+    setName("");
+    setIsbn("");
+    setAuthor("");
+    setEditorial("");
+  };
+
   return (
     <>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div
           style={{
             display: "flex",
             gap: 12,
             padding: 12,
+            justifyContent: "center",
           }}
         >
-          <input type="text" placeholder="Nombre" />
-          <input type="text" placeholder="ISBN" />
-          <input type="text" placeholder="Autor" />
-          <input type="text" placeholder="Editorial" />
+          <input
+            type="text"
+            placeholder="Nombre"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="ISBN"
+            value={isbn}
+            onChange={(e) => setIsbn(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Autor"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Editorial"
+            value={editorial}
+            onChange={(e) => setEditorial(e.target.value)}
+          />
         </div>
         <div
           style={{
@@ -22,8 +90,10 @@ export function BookForm() {
             justifyContent: "center",
           }}
         >
-          <button>Agregar Libro</button>
-          <button>Tomar Libro</button>
+          <button type="submit">Agregar Libro</button>
+          <button type="button" onClick={takeBook}>
+            Tomar Libro
+          </button>
         </div>
       </form>
     </>
