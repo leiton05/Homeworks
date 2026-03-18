1c210eb96779ba0c2c1ff3c27e2dbef59078162e
leiton <sebastian.leiton@uao.edu.co>
2026-03-17 21:50:02 -0500

    ✨Feat: Implement List logic to display every object in the array

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,11 +1,29 @@
-import type { Book } from "../models/Book"
+import type { Book } from "../models/Book";
+import Stack from "../tools/Stack";
 
-export function BookList(book: Book) {
-    return (
-        <>
-            <ol>
-                
-            </ol>
-        </>
-    )
+interface BookListProps {
+  book: Stack<Book>;
+}
+
+export function BookList({ book }: BookListProps) {
+  const books = book.getItems();
+
+  /* Espero que este HTML sea decente, porque no sé mucho de HTML */
+  return (
+    <>
+      <ol>
+        {books.map((book, index) => (
+          <li key={index}>
+            <div>
+              {book.name} - Autor {book.author}
+            </div>
+            <div>
+              ISBN: {book.isbn} - Editorial: {book.editorial}
+            </div>
+            <p></p>
+          </li>
+        ))}
+      </ol>
+    </>
+  );
 }
