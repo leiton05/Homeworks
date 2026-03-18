c1d411c15ea0f8da86ba06c00312b5a990b7431b
leiton <sebastian.leiton@uao.edu.co>
2026-03-18 01:45:01 -0500

    🚧Construction: Start structuring the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import "./App.css";
+import { ClientForm } from "./components/ClientForm";
+import type { Client } from "./models/Client";
+import Queue from "./tools/Queue";
 
 function App() {
+  const [queue, setQueue] = useState(new Queue<Client>());
+  /* for (let i = 0; i < 5; i++) {
+    console.log(RandomDateBeforeNow());
+  } */
+
   return (
     <>
       <h1>QUEUE</h1>
+      <ClientForm queue={queue} setQueue={setQueue} />
     </>
   );
 }
