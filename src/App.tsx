0d7b5c2e89a5d475c43541444fa45695e97d54de
leiton <sebastian.leiton@uao.edu.co>
2026-03-18 00:11:45 -0500

    ♻️Fix: Fix useless imports and finished the HTML structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 import { BookForm } from "./components/BookForm";
 import { BookList } from "./components/BookList";
 import { mockData } from "./data/mockData";
 import type { Book } from "./models/Book";
-import type Stack from "./tools/Stack";
-import { ArrayToStacks } from "./tools/StackMethods";
+import Stack from "./tools/Stack";
 
 /* console.log(mockData) */
 
-/* Convierto el Arreglo de la mockData de los libros y lo paso 
-a que sea un Stack */
-const BookStack: Stack<Book> = ArrayToStacks(mockData);
-console.log(BookStack.print());
-
 function App() {
+  const [stack, setStack] = useState(new Stack<Book>(mockData));
   return (
     <>
       <h1>BOOKSTACKS</h1>
       <p>Coloca tus libros para luego facilmente sacarlos</p>
-      <BookForm />
-      <h3>Libros en espera:</h3>
-      <BookList book={BookStack} />
+      <BookForm stack={stack} setStack={setStack} />
+      <div
+        style={{
+          backgroundColor: "#3d3d3d",
+          padding: 13,
+          margin: 13,
+          borderRadius: 16,
+        }}
+      >
+        <h3>Libros en espera:</h3>
+        <BookList book={stack} />
+      </div>
     </>
   );
 }
