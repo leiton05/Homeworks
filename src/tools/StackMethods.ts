9491532dfec39b892b02e0c179975234eb732078
leiton <sebastian.leiton@uao.edu.co>
2026-03-17 21:46:12 -0500

    ✨Feat: Implement ArrayToStacks function

--- src/tools/StackMethods.ts
+++ src/tools/StackMethods.ts
@@ -1,5 +1,14 @@
-import type Stack from "./Stack";
+import type { Book } from "../models/Book";
+import Stack from "./Stack";
 
-function ArrayToStacks(array: Stack[]) {
-    return array.map(()=>console.log(""))
+/* Basicamente esta función permite pasar de un arreglo de 
+objetos Book a un stack de objetos Book */
+export function ArrayToStacks(array: Array<Book>) {
+  const BooksStack: Stack<Book> = new Stack();
+
+  array.forEach((element) => {
+    BooksStack.push(element);
+  });
+
+  return BooksStack;
 }
