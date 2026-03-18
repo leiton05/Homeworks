5f2e5d02668e60e20e9e824e5e56fa3d2eb29ede
leiton <sebastian.leiton@uao.edu.co>
2026-03-17 19:46:34 -0500

    ✨Feat: Create BookForm file to render the list of Books

--- src/components/BookForm.tsx
+++ src/components/BookForm.tsx
@@ -2,23 +2,37 @@ export function BookForm() {
     return (
         <>
             <form action="">
-                <input 
-                type="text"
-                placeholder="Nombre"
-                />
-                <input 
-                type="text"
-                placeholder="ISBN"
-                />
-                <input 
-                type="text"
-                placeholder="Autor"
-                />
-                <input 
-                type="text"
-                placeholder="Editorial"
-                />
-                <button> Agregar </button>
+                <div style={{
+                    display: "flex",
+                    gap: 12,
+                    padding: 12
+                }}>
+                    <input 
+                    type="text"
+                    placeholder="Nombre"
+                    />
+                    <input 
+                    type="text"
+                    placeholder="ISBN"
+                    />
+                    <input 
+                    type="text"
+                    placeholder="Autor"
+                    />
+                    <input 
+                    type="text"
+                    placeholder="Editorial"
+                    />
+                </div>
+                <div style = {{
+                    display: "flex",
+                    gap: 12,
+                    padding: 12,
+                    justifyContent: "center"
+                }}>
+                    <button>Agregar Libro</button>
+                    <button>Tomar Libro</button>
+                </div>
             </form>
         </>
     )
