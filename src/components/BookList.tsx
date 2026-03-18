173aca06696b31799448c3788d9fa96edd94bd33
leiton <sebastian.leiton@uao.edu.co>
2026-03-17 19:49:16 -0500

    ✨Feat: Start the HTML structure of the list to display de books

---
+++ src/components/BookList.tsx
@@ -0,0 +1,11 @@
+import type { Book } from "../models/Book"
+
+export function BookList(book: Book) {
+    return (
+        <>
+            <ol>
+                
+            </ol>
+        </>
+    )
+}
