c6397c553fa4144fee5078c92edc3f9ef6464ead
leiton <sebastian.leiton@uao.edu.co>
2026-03-18 01:50:39 -0500

    ✨Feat: Add function to generate a Random date before the actual one

---
+++ src/tools/Util.ts
@@ -0,0 +1,8 @@
+export function RandomDateBeforeNow() {
+  const actualDate = new Date();
+  const randomSeconds = Math.random() * 30 * 24 * 60 * 60;
+
+  const randomDate = new Date(actualDate.getTime() - randomSeconds);
+
+  return randomDate;
+}
