1881422608c4e9651f20860e118f7d6b76788a94
leiton <sebastian.leiton@uao.edu.co>
2026-03-17 21:48:38 -0500

    🎨Format: Improve the indentation of the file

--- src/components/BookForm.tsx
+++ src/components/BookForm.tsx
@@ -1,39 +1,31 @@
 export function BookForm() {
-    return (
-        <>
-            <form action="">
-                <div style={{
-                    display: "flex",
-                    gap: 12,
-                    padding: 12
-                }}>
-                    <input 
-                    type="text"
-                    placeholder="Nombre"
-                    />
-                    <input 
-                    type="text"
-                    placeholder="ISBN"
-                    />
-                    <input 
-                    type="text"
-                    placeholder="Autor"
-                    />
-                    <input 
-                    type="text"
-                    placeholder="Editorial"
-                    />
-                </div>
-                <div style = {{
-                    display: "flex",
-                    gap: 12,
-                    padding: 12,
-                    justifyContent: "center"
-                }}>
-                    <button>Agregar Libro</button>
-                    <button>Tomar Libro</button>
-                </div>
-            </form>
-        </>
-    )
+  return (
+    <>
+      <form action="">
+        <div
+          style={{
+            display: "flex",
+            gap: 12,
+            padding: 12,
+          }}
+        >
+          <input type="text" placeholder="Nombre" />
+          <input type="text" placeholder="ISBN" />
+          <input type="text" placeholder="Autor" />
+          <input type="text" placeholder="Editorial" />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            gap: 12,
+            padding: 12,
+            justifyContent: "center",
+          }}
+        >
+          <button>Agregar Libro</button>
+          <button>Tomar Libro</button>
+        </div>
+      </form>
+    </>
+  );
 }
