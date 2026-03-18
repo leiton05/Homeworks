7329c4a3b46aaecf1c2ea5600288c1335244a34d
leiton <sebastian.leiton@uao.edu.co>
2026-03-17 21:47:12 -0500

    ✨Feat: Create getItems method to get the propertie items[]

--- src/tools/Stack.ts
+++ src/tools/Stack.ts
@@ -1,36 +1,43 @@
-class Stack<item> { 
-    private items: item[];
-    constructor() {
-        this.items = [];
-    }
+class Stack<item> {
+  private items: item[];
+  constructor() {
+    this.items = [];
+  }
 
-    push(value: item): void {
-        this.items.push(value);
-    }
+  push(value: item): void {
+    this.items.push(value);
+  }
 
-    pop(): item | null {
-        return this.items.length > 0 ? this.items.pop()! : null;
-    }
+  pop(): item | null {
+    return this.items.length > 0 ? this.items.pop()! : null;
+  }
 
-    peek(): item | null {
-        return this.items.length > 0 ? this.items[this.items.length - 1] : null;
-    }
+  peek(): item | null {
+    return this.items.length > 0 ? this.items[this.items.length - 1] : null;
+  }
 
-    isEmpty(): boolean {
-        return this.items.length === 0;
-    }
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
 
-    size(): number {
-        return this.items.length;
-    }
+  size(): number {
+    return this.items.length;
+  }
 
-    print(): void {
-        this.items.slice().reverse().forEach(
-            (item: item) => {
-                console.log(item)
-            }
-        )
-    }
+  print(): void {
+    this.items
+      .slice()
+      .reverse()
+      .forEach((item: item) => {
+        console.log(item);
+      });
+  }
+
+  /* Agregé este metodo para devolver la propiedad de 
+  this.items[] */
+  getItems(): item[] {
+    return [...this.items];
+  }
 }
 
 export default Stack;
