339ddc60a4577abb32316caa83f4526a62383925
leiton <sebastian.leiton@uao.edu.co>
2026-03-22 03:00:57 -0500

    ✨Feat: Declare the main routes in the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Challenge04 from "./pages/Challenge04";
+import Challenge05 from "./pages/Challenge05";
+import { Home } from "./pages/Home";
+
 function App() {
   return (
     <>
-      <h1>CHALLENGE 06</h1>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/c4"} element={<Challenge04 />} />
+        <Route path={"/c5"} element={<Challenge05 />} />
+      </Routes>
     </>
   );
 }
