dda84f592f4229f3610536591ec60a9c64dc86f5
leiton <sebastian.leiton@uao.edu.co>
2026-03-23 01:03:05 -0500

    ✨Feat: Implement logic to hide buttons when its access is no available

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,26 @@
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+import { LogoutButton } from "../components/LogOutButton";
 
 export function Home() {
   const navigate = useNavigate();
+  const { isLoggedIn } = useAuth();
   return (
     <>
       <h1>PAGINA DE INICIO</h1>
-      <button onClick={() => navigate("/login")}>Login</button>
-      <button onClick={() => navigate("/c4")}>Challenge 04</button>
-      <button onClick={() => navigate("/c5")}>Challenge 05</button>
+      {isLoggedIn ? null : (
+        <button onClick={() => navigate("/login")}>Login</button>
+      )}
+
+      {isLoggedIn ? (
+        <button onClick={() => navigate("/c4")}>Challenge 04</button>
+      ) : null}
+
+      {isLoggedIn ? (
+        <button onClick={() => navigate("/c5")}>Challenge 05</button>
+      ) : null}
+
+      {isLoggedIn ? <LogoutButton /> : null}
     </>
   );
 }
