a595d78664292638b4dff108699c78d3b254739b
leiton <sebastian.leiton@uao.edu.co>
2026-03-22 21:25:55 -0500

    ✨Feat: Create Login file

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,10 @@
+function LoginPage() {
+  return (
+    <>
+      <h1>PAGINA DE LOGIN</h1>
+      <p>Bienvenido a la pagina de Login, ingrese sus datos</p>
+    </>
+  );
+}
+
+export default LoginPage;
