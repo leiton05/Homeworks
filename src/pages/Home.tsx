679e7d912b11806858f82bf02248be965ccb81e6
leiton <sebastian.leiton@uao.edu.co>
2026-03-22 21:27:27 -0500

    ✨Feat: Add button to go to the Login page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,7 @@ export function Home() {
   return (
     <>
       <h1>PAGINA DE INICIO</h1>
+      <button onClick={() => navigate("/login")}>Login</button>
       <button onClick={() => navigate("/c4")}>Challenge 04</button>
       <button onClick={() => navigate("/c5")}>Challenge 05</button>
     </>
