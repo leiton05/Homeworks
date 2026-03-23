e85d7435933ab8e27282eb0916f16d7b08597b04
leiton <sebastian.leiton@uao.edu.co>
2026-03-23 00:45:18 -0500

    ✨Feat: Separate the private routes to the public routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import Challenge04 from "./pages/Challenge04";
 import Challenge05 from "./pages/Challenge05";
 import { Home } from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 function App() {
   return (
     <>
       <Routes>
+        {/* Rutas publicas */}
         <Route path={"/"} element={<Home />} />
         <Route path={"/login"} element={<LoginPage />} />
-        <Route path={"/c4"} element={<Challenge04 />} />
-        <Route path={"/c5"} element={<Challenge05 />} />
+
+        {/* Rutas privadas */}
+        <Route element={<PrivateRoute />}>
+          <Route path={"/c4"} element={<Challenge04 />} />
+          <Route path={"/c5"} element={<Challenge05 />} />
+        </Route>
       </Routes>
     </>
   );
