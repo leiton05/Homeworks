960260cc5a386acbc85926765eb46ad23e498221
leiton <sebastian.leiton@uao.edu.co>
2026-03-22 21:28:21 -0500

    ✨Feat: Add Login route

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Challenge04 from "./pages/Challenge04";
 import Challenge05 from "./pages/Challenge05";
 import { Home } from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path={"/"} element={<Home />} />
+        <Route path={"/login"} element={<LoginPage />} />
         <Route path={"/c4"} element={<Challenge04 />} />
         <Route path={"/c5"} element={<Challenge05 />} />
       </Routes>
