1a84ec4753b4ce7dc744ea89278026fc169ac307
leiton <sebastian.leiton@uao.edu.co>
2026-04-06 23:10:44 -0500

    ✨ Feat: Add a context to manage Auth logic and remove MyContext.tsx

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,26 @@
+import { createContext, type ReactNode } from "react";
+import type { UserNotPass, UserLogin, User } from "../models/userType";
+import useAuthState from "../hooks/useAuthState";
+
+interface AuthContextProps {
+  user: UserNotPass | null;
+  loading: boolean;
+  registerWithEmailAndPassword: (userInfo: User) => Promise<string | null>;
+  loginWithEmailAndPassword: (userInfo: UserLogin) => Promise<string | null>;
+  logout: () => Promise<string | null>;
+  getUserId: () => string | undefined;
+}
+
+interface Provider {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext<AuthContextProps | null>(null);
+
+export const AuthContextProvider = ({ children }: Provider) => {
+  const contextData = useAuthState();
+
+  return (
+    <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
+  );
+};
