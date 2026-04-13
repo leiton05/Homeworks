a88e5c25ad7c2bf84073a3b131b72d487b2dff48
leiton <sebastian.leiton@uao.edu.co>
2026-04-13 02:07:15 -0500

    ✨ Feat: Implement the checkValueInTree function

--- src/tools/tree/TreeUtils.ts
+++ src/tools/tree/TreeUtils.ts
@@ -1,4 +1,5 @@
 import BinaryTree from "./BinaryTree";
+import type Node from "./Node";
 
 export function stringToTree(numbers: string) {
   const arrayNumbers = numbers
@@ -11,4 +12,15 @@ export function stringToTree(numbers: string) {
     numbersTree.insert(arrayNumbers[i]);
   }
   return numbersTree;
+}
+
+export function checkValueInTree(
+  node: Node<number> | null,
+  number: number,
+): boolean {
+  if (!node) return false;
+  if (number === node.value) return true;
+  return (
+    checkValueInTree(node.left, number) || checkValueInTree(node.right, number)
+  );
 }
