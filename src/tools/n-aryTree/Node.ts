b56252d61bb0a713b5f525ba0de69c39b92ab228
leiton <sebastian.leiton@uao.edu.co>
2026-04-14 01:14:29 -0500

    ✨ Feat: Implement the necessary models of N-ary Tree

---
+++ src/tools/n-aryTree/Node.ts
@@ -0,0 +1,14 @@
+class Node<T> {
+  value: T;
+  children: Node<T>[];
+  constructor(value: T) {
+    this.value = value;
+    this.children = [];
+  }
+
+  addChild(node: Node<T>) {
+    this.children.push(node);
+  }
+}
+
+export default Node;
