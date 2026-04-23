0f682047743d89741719c851792b08cd715128b9
leiton <sebastian.leiton@uao.edu.co>
2026-04-22 22:17:28 -0500

    ✨ Feat: Implement basic routes of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
+import Graph from "./pages/Graph";
+
 function App() {
   return (
-    <div>
-      <h1>Challenge 10</h1>
-    </div>
+    <>
+      <Routes>
+        {/* Rutas publicas */}
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/graph"} element={<Graph />} />
+
+        {/* Rutas no encontradas */}
+        <Route path={"*"} element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
