72978353bb264dc057244c75c0b9f73f507a7e01
leiton <sebastian.leiton@uao.edu.co>
2026-05-27 16:36:39 -0500

    ✨ Feat: Update the dashboard to let the user decide what to explore

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,15 +1,26 @@
 import Navbar from "@/shared/components/ui/navigation/Navbar";
-import { Header } from "@/shared/components/ui/sections/Header";
 import logo from "@/assets/logo.svg";
+import { BGCard } from "@/shared/components/ui/elements/BGCard";
 
 export const Dashboard = () => {
   return (
     <>
       <Navbar imgUrl={logo} title="Aura Music" alt="Logo de aura music" />
-      <Header
-        title="Bienvenido/a a Aura Music"
-        paragraph="Explora, añade y disfruta de tus canciones favoritas"
-      />
+      <div className="flex flex-col items-center justify-center">
+        <h2>Es hora de escoger que quieres explorar</h2>
+      </div>
+      <div className="flex justify-center items-center gap-10 mt-16">
+        <BGCard
+          description="Explora nuestra colección de música"
+          buttonTag="Explorar"
+          link="/explore"
+        />
+        <BGCard
+          description="Descubre que canciones se encuentra en tendencia"
+          buttonTag="Ver en tendencia"
+          link="/recommendations"
+        />
+      </div>
     </>
   );
 };
